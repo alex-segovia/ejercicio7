c2a8f56483cebc7b3231a1c1b36c50ec785f4e98
May3012 <139161004+May3012@users.noreply.github.com>
2024-05-11 22:34:43 -0500

    Character Repository

--- src/main/java/com/example/ejercicio7/Repository/CharacterRepository.java
+++ src/main/java/com/example/ejercicio7/Repository/CharacterRepository.java
@@ -1,4 +1,34 @@
 package com.example.ejercicio7.Repository;
 
-public interface CharacterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character,Integer> {
+    @Query(nativeQuery = true,value = "select distinct(identity) from characters where identity is not null and identity != ' '")
+    List<String> listarIdentity();
+
+    @Query(nativeQuery = true,value = "select distinct(align) from characters where align is not null and align != ' '")
+    List<String> listarAlineacion();
+
+    @Query(nativeQuery = true,value = "select distinct(eye) from characters where eye is not null and eye != ' '")
+    List<String> listarOjos();
+
+    @Query(nativeQuery = true,value = "select distinct(hair) from characters where hair is not null and hair != ' '")
+    List<String> listarCabello();
+
+    @Query(nativeQuery = true,value = "select distinct(sex) from characters where sex is not null and sex != ' '")
+    List<String> listarSexo();
+
+    @Query(nativeQuery = true,value = "select distinct(gsm) from characters where gsm is not null and gsm != ' '")
+    List<String> listarGSM();
+
+    @Query(nativeQuery = true,value = "select distinct(alive) from characters where alive is not null and alive != ' '")
+    List<String> listarAlive();
+
+    @Query(nativeQuery = true, value = "select * from characters where name like ?1")
+    List<Character> findByNameIgnoreCaseContaining(String busqueda);
 }
